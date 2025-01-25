e2ca8277c2c58a50392a79d44225e9c6b189effe
Christoph Souris <christoph.souris@gmail.com>
2025-01-25 16:51:26 +0100

    Fixing tests and code to match latest name/test handling in Cumulocity cloud.

--- c8y_api/model/_util.py
+++ c8y_api/model/_util.py
@@ -3,6 +3,7 @@
 # and/or its subsidiaries and/or its affiliates and/or their licensors.
 # Use, reproduction, transfer, publication or disclosure is prohibited except
 # as specifically provided for in your License Agreement with Software AG.
+
 import re
 from datetime import datetime, timedelta, timezone
 from dateutil import parser
@@ -32,6 +33,14 @@ class _QueryUtil(object):
         # single quotes escaped through single quote
         return sub('\'', '\'\'', value)
 
+    @staticmethod
+    def encode_odata_text_value(value):
+        """Encode value strings according to OData query rules.
+        http://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_URLParsing
+        http://docs.oasis-open.org/odata/odata/v4.01/cs01/abnf/odata-abnf-construction-rules.txt """
+        # single quotes escaped through single quote
+        encoded_quotes = sub('\'', '\'\'', value)
+        return encoded_quotes if " " not in encoded_quotes else f"'{encoded_quotes}'"
 
 class _DateUtil(object):
 
