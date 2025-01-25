7ccfda47d558371c9ceb693c7fe0ab53d5ca30b8
Christoph Souris <christoph.souris@gmail.com>
2025-01-25 16:44:50 +0100

    Added 'init' and 'test' targets including possibility to run tests in docker (3.7 and 3.11 versions of Python).

--- tasks.py
+++ tasks.py
@@ -4,9 +4,45 @@
 # Use, reproduction, transfer, publication or disclosure is prohibited except
 # as specifically provided for in your License Agreement with Software AG.
 
+from getpass import getpass
 from invoke import task
 
 import util.microservice_util as ms_util
+
+
+@task
+def init(c):
+    """Initialize/setup the development environment."""
+    print(("Setting up connection to Cumulocity development environment.\n"
+           "(TOTP protected accouts are not supported)"))
+    host, tenant, user, password = (
+        input("Hostname: "),
+        input("Tenant:   "),
+        input("Username: "),
+        getpass("Password: "),
+    )
+    print("Optional: device bootstrapping credentials (leave blank to skip).")
+    bootstrap_tenant, bootstrap_user, bootstrap_password = (
+        input(f"Tenant (default: management):        "),
+        input(f"Username (default: devicebootstrap): "),
+        getpass("Password: "),
+    )
+
+    with open('.env', 'wt') as file:
+        file.writelines([
+            f'C8Y_BASEURL={host}\n',
+            f'C8Y_TENANT={tenant}\n',
+            f'C8Y_USER={user}\n',
+            f'C8Y_PASSWORD={password}\n',
+            '\n',
+            f'C8Y_BOOTSTRAP_TENANT={tenant}\n',
+            f'C8Y_BOOTSTRAP_USER={user}\n',
+            f'C8Y_BOOTSTRAP_PASSWORD={password}\n',
+            '\n',
+            f'C8Y_DEVICEBOOTSTRAP_TENANT={bootstrap_tenant or "management"}\n',
+            f'C8Y_DEVICEBOOTSTRAP_USER={bootstrap_user or "devicebootstrap"}\n',
+            f'C8Y_DEVICEBOOTSTRAP_PASSWORD={bootstrap_password or ""}\n',
+        ])
 
 
 @task(help={
@@ -18,6 +54,34 @@ def lint(c, scope='all'):
     if scope == 'all':
         scope = 'c8y_api c8y_tk tests integration_tests samples'
     c.run(f'pylint --rcfile pylintrc --fail-under=9 {scope}')
+
+
+@task(help={
+    'scope': "Which tests to run, e.g. 'tests.model'"
+             "Default: 'tests'",
+    'python': ("Whether to run tests within a docker container and which"
+               "Python version to use - 3.7 or 3.11. Default: None")
+})
+def test(c, scope='tests', python=None):
+
+    docker_name = None
+    if python == '3.7':
+        docker_name = 'buster37'
+    elif python == '3.11':
+        docker_name = 'bookworm311'
+    if docker_name:
+        dockerfile = f'{docker_name}.dockerfile'
+        cmd_build = f'docker build -f {dockerfile} -t {docker_name} .'
+        print(f"Executing '{cmd_build}' ...")
+        c.run(cmd_build, pty=True)
+        cmd_run = ('docker run --rm -it -v $(pwd):/code --env-file .env '
+                   f'{docker_name} bash -c "cd /code '
+                   '&& pip show c8y_api || pip install -e . '
+                   f'&& pytest -W ignore::DeprecationWarning {scope}"')
+        print(f"Executing '{cmd_run}' ...")
+        c.run(cmd_run, pty=True)
+    else:
+        c.run(f"pytest -W ignore::DeprecationWarning {scope}")
 
 
 @task
