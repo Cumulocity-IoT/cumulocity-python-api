cbd30c8590c3b10a498f4fc59230fb05c1a728dc
Christoph Souris <christoph.souris@cumulocity.com>
2025-07-31 16:17:34 +0200

    Added some assertion helpers and optimized function isolate_last_call_arg and isolate_all_call_args.

--- tests/utils.py
+++ tests/utils.py
@@ -14,18 +14,41 @@ from c8y_api.model._base import CumulocityObject
 from util.testing_util import RandomNameGenerator
 
 
+def assert_in_any(string, *strings):
+    """Assert that a string is in any of a given list of strings."""
+    assert any(string in s for s in strings), f"'{string}' is not in any of {strings}"
+
+
+def assert_not_in_any(string, *strings):
+    """Assert that a string is not in any of a given list of strings."""
+    assert all(string not in s for s in strings), f"'{string}' is in one of {strings}"
+
+
+def assert_all_in_any(multiple_strings, *strings):
+    """Assert that multiple strings are in a given list of strings."""
+    for string in multiple_strings:
+        assert any(string in s for s in strings), f"'{string}' is not in any of {strings}"
+
+
+def assert_all_not_in_any(multiple_strings, *strings):
+    """Assert that multiple strings are in a given list of strings."""
+    for string in multiple_strings:
+        assert all(string not in s for s in strings), f"'{string}' is in one of {strings}"
+
+
 def get_ids(objs: List[CumulocityObject]) -> Set[str]:
     """Isolate the ID from a list of database objects."""
     return {o.id for o in objs}
 
 
-def isolate_last_call_arg(mock: Mock, name: str, pos: int = None) -> Any:
+def isolate_last_call_arg(mock: Mock, *args: str | int, name: str = None, pos: int = None) -> Any:
     """Isolate arguments of the last call to a mock.
 
-    The argument can be specified by name and by position.
+    The argument can be specified by name and by position, in any order or by a named parameter.
 
     Args:
         mock (Mock): the Mock to inspect
+        args (str|int): the argument to isolate (by name, position or both)
         name (str): Name of the parameter
         pos (int): Position of the parameter
 
@@ -37,22 +60,30 @@ def isolate_last_call_arg(mock: Mock, name: str, pos: int = None) -> Any:
             position was not given/out of bounds.
     """
     mock.assert_called()
-    args, kwargs = mock.call_args
-    if name in kwargs:
-        return kwargs[name]
-    if len(args) > pos:
-        return args[pos]
+    call_args, call_kwargs = mock.call_args
+    name = name or None
+    pos = pos if pos is not None else -1
+    for arg in args:
+        if isinstance(arg, int):
+            pos = arg
+        elif isinstance(arg, str):
+            name = arg
+    if name in call_kwargs:
+        return call_kwargs[name]
+    if len(call_args) > pos:
+        return call_args[pos]
     raise KeyError(f"Argument not found: '{name}'. "
                    f"Not given explicitly and position ({pos}) out of of bounds.")
 
 
-def isolate_all_call_args(mock: Mock, name: str, pos: int = None) -> List[Any]:
+def isolate_all_call_args(mock: Mock, *args: str | int, name: str, pos: int = None) -> List[Any]:
     """Isolate arguments of all calls to a mock.
 
-    The argument can be specified by name and by position.
+    The argument can be specified by name and by position, in any order or by a named parameter.
 
     Args:
         mock (Mock): the Mock to inspect
+        args (str|int): the argument to isolate (by name, position or both)
         name (str): Name of the parameter
         pos (int): Position of the parameter
 
@@ -64,12 +95,19 @@ def isolate_all_call_args(mock: Mock, name: str, pos: int = None) -> List[Any]:
             position was not given/out of bounds.
     """
     mock.assert_called()
+    name = name or None
+    pos = pos if pos is not None else -1
+    for arg in args:
+        if isinstance(arg, int):
+            pos = arg
+        elif isinstance(arg, str):
+            name = arg
     result = []
-    for args, kwargs in mock.call_args_list:
-        if name in kwargs:
-            result.append(kwargs[name])
-        elif len(args) > pos:
-            result.append(args[pos])
+    for call_args, call_kwargs in mock.call_args_list:
+        if name in call_kwargs:
+            result.append(call_kwargs[name])
+        elif len(call_args) > pos:
+            result.append(call_args[pos])
     if not result:
         raise KeyError(f"Argument not found in any of the calls: '{name}', pos: {pos}.")
     return result
